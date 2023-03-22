d97c68ed60c03d92550e0685b50ab77ea9f5baa1
Gigilka <kuchin-02@mail.ru>
2023-03-22 17:09:06 +0300

    style cng

--- modules/task_1/kuchin_n_ccs_matrix/ccs_matrix.h
+++ modules/task_1/kuchin_n_ccs_matrix/ccs_matrix.h
@@ -1,6 +1,6 @@
 // Copyright 2023 Kuchin Nikita
-#ifndef MODULES_TASK_1_KUCHIN_N_CCS_MATIX_CCS_MATRIX_H_
-#define MODULES_TASK_1_KUCHIN_N_CCS_MATIX_CCS_MATRIX_H_
+#ifndef MODULES_TASK_1_KUCHIN_N_CCS_MATRIX_CCS_MATRIX_H_
+#define MODULES_TASK_1_KUCHIN_N_CCS_MATRIX_CCS_MATRIX_H_
 
 #include <vector>
 struct SparceMatrix {
@@ -9,4 +9,4 @@ struct SparceMatrix {
     std::vector<int> col_ptr;
 };
 
-#endif  // MODULES_TASK_1_KUCHIN_N_CCS_MATIX_CCS_MATRIX_H_
+#endif  // MODULES_TASK_1_KUCHIN_N_CCS_MATRIX_CCS_MATRIX_H_
