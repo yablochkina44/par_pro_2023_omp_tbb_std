b8baf53e0ddc93a50f9859c5389f2120aaf83b87
YoungPinch <dreamerandrew17@gmail.com>
2023-03-24 21:21:14 +0300

    StyleFix

--- modules/task_1/smirnov_a_gaussian_image_filter/gaussian_image_filter.cpp
+++ modules/task_1/smirnov_a_gaussian_image_filter/gaussian_image_filter.cpp
@@ -6,31 +6,31 @@ vector<intensityType> genImage(int rows, int columns) {
   std::mt19937 gen(dev());
   vector<intensityType> image(rows * columns);
   for (size_t i = 0; i < rows * columns; ++i) {
-	image[i] = gen() % 256;
+    image[i] = gen() % 256;
   }
   return image;
 }
 
 vector<intensityType> gaussianFilterSeq(const vector<intensityType>& image, int rows, int columns) {
-  if(image.size() != rows * columns)
-	throw std::string("Error with values rows or columns");
+  if (image.size() != rows * columns)
+    throw std::string("Error with values rows or columns");
   vector<intensityType> resultImage(rows * columns);
   char radius = kernelSize / 2;
   for (int i = 0; i < rows; ++i) {
-	for (int j = 0; j < columns; ++j) {
-	  int curPixIntens = 0;
-	  char gausMatrixIndex = 0;
-	  for (int xKernel = -radius; xKernel <= radius; ++xKernel) {
-		for (int yKernel = -radius; yKernel <= radius; ++yKernel) {
-		  int neighboorPixerX = std::max(0, std::min(i + xKernel, rows - 1));
-		  int neighboorPixelY = stsd::max(0, std::min(j + yKernel, columns -1));
-		  curPixIntens += image[neighboorPixelX * columns + neighboorPixelY]
-			* kernel[gausMatrixIndex];
-		  gausMatrixIndex++;
-		}
-	  }
-	  resultImage[i * columns + j] = curPixIntens / kernelSum;
-	}
+    for (int j = 0; j < columns; ++j) {
+      int curPixIntens = 0;
+      char gausMatrixIndex = 0;
+      for (int xKernel = -radius; xKernel <= radius; ++xKernel) {
+        for (int yKernel = -radius; yKernel <= radius; ++yKernel) {
+          int neighboorPixerX = std::max(0, std::min(i + xKernel, rows - 1));
+          int neighboorPixelY = stsd::max(0, std::min(j + yKernel, columns - 1));
+          curPixIntens += image[neighboorPixelX * columns + neighboorPixelY]
+            * kernel[gausMatrixIndex];
+          gausMatrixIndex++;
+        }
+      }
+      resultImage[i * columns + j] = curPixIntens / kernelSum;
+    }
   }
   return resultImage;
 }
