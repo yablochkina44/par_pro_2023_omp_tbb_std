c94697871370ec8b2d37f9a301eb489be24ef9e3
TiaYeah <nikita58316@mail.ru>
2023-03-29 22:27:05 +0300

    Fix2

--- modules/task_1/kudryashov_n_gauss_vertical/kudryashov_n_gauss_vertical.h
+++ modules/task_1/kudryashov_n_gauss_vertical/kudryashov_n_gauss_vertical.h
@@ -10,4 +10,4 @@ std::vector<std::vector<double>> createKernel();
 int calcNewPixelColor(std::vector<std::vector<int>> image, int height,
   int width, int y, int x, std::vector<std::vector<double>> kernel);
 std::vector<std::vector<int>> gauss_filter_sequence(
-  const std::vector<std::vector<int>>& image image, int height, int width);
+  const std::vector<std::vector<int>>& image, int height, int width);
