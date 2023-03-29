18ee53680fc5401053fa24b77ed167a8aa8781f6
StJi <denproko@yandex.ru>
2023-03-30 00:27:48 +0300

    upd

--- modules/task_1/prokofev_strassen_double/main.cpp
+++ modules/task_1/prokofev_strassen_double/main.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 
 
-TEST(int_test, int_test) {
+TEST(first_test, first_test) {
     std::vector<double> a = { 1.0, 2.0, 3.0, 4.0 };
     std::vector<double> b = { 1.0, 2.0, 3.0, 4.0 };
     std::vector<double> c = { 7.0, 10.0, 15.0, 22.0 };
@@ -12,26 +12,22 @@ TEST(int_test, int_test) {
     EXPECT_EQ(sMult(a,b),c);
 }
 
-TEST(seqSobelFilter, randMatrixWithNullSize) {
-    ASSERT_ANY_THROW(randImage(0, 0));
+TEST(2test, 2test) {
+    std::vector<double> a = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0 };
+    std::vector<double> b = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0 };
+    std::vector<double> c = { 30.0, 36.0, 42.0, 66.0, 81.0, 96.0, 102.0, 126.0, 150 };
+
+    EXPECT_EQ(sMult(a,b),c);
 }
 
-TEST(seqSobelFilter, emptyMatrix) {
-    ASSERT_ANY_THROW(image<uint8_t>(0, 0));
+TEST(3test, 3test) {
+    std::vector<double> a = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9 };
+    std::vector<double> b = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9 };
+    std::vector<double> c = { 36.3, 43.56, 50.82, 79.86, 98.01, 116.16, 123.42, 152.46, 181.5 };
+
+    EXPECT_EQ(sMult(a,b),c);
 }
 
-TEST(seqSobelFilter, sobelNoThrow) {
-    auto img = randImage(5, 5);
-    ASSERT_NO_THROW(sobelSequence(img));
-}
-
-TEST(seqSobelFilter, sobelChangeMatrix) {
-    auto img = randImage(5, 5);
-    auto res = sobelSequence(img);
-    ASSERT_EQ(img._columns, res._columns);
-    ASSERT_EQ(img._rows, res._rows);
-    ASSERT_NE(img._matrix, res._matrix);
-}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
