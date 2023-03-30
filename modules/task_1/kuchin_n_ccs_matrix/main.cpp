42bfa3c65ffa49cc4bcb6172b100c6a810f81933
gigilka <kuchin-02@mail.ru>
2023-03-31 02:45:28 +0300

    add test

--- modules/task_1/kuchin_n_ccs_matrix/main.cpp
+++ modules/task_1/kuchin_n_ccs_matrix/main.cpp
@@ -20,7 +20,21 @@ TEST(Str, test1) {
     EXPECT_EQ(B.col_ptr, C.col_ptr);
 }
 
-TEST(Str, test2) { EXPECT_EQ(1, 1); }
+TEST(Str, test2) {
+    SparceMatrix A;
+    A.n = 4;
+    A.data = {2.37, 3.65, 1.22, 7.63, 4.22};
+    A.row_id = {0, 2, 3, 3, 0};
+    A.col_ptr = {0, 2, 3, 3, 4, 5};
+    SparceMatrix B;
+    B.data = {2.37, 4.22, 3.65, 4.22, 7.63};
+    B.row_id = {0, 4, 0, 1, 3};
+    B.col_ptr = {0, 2, 2, 3, 5, 5};
+    SparceMatrix C = transport(A);
+    EXPECT_EQ(B.data, C.data);
+    EXPECT_EQ(B.row_id, C.row_id);
+    EXPECT_EQ(B.col_ptr, C.col_ptr);
+}
 
 TEST(Str, test3) { EXPECT_EQ(1, 1); }
 TEST(Str, test4) { EXPECT_EQ(1, 1); }
