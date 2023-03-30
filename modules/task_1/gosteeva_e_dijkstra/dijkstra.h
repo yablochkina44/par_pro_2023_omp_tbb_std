e060087c664699090d66cf49dfb7ec116172360a
Kat-G <katylaky@outlook.com>
2023-03-30 14:43:20 +0300

    c1

---
+++ modules/task_1/gosteeva_e_dijkstra/dijkstra.h
@@ -0,0 +1,10 @@
+// Copyright 2023 Gosteeva Ekaterina
+#ifndef MODULES_TASK_1_GOSTEEVA_E_DIJKSTRA_DIJKSTRA_H
+#define MODULES_TASK_1_GOSTEEVA_E_DIJKSTRA_DIJKSTRA_H
+
+#include <climits>     
+#include <vector>
+
+std::vector<int> Dijkstra(std::vector<int> graph, int source,int size);
+
+#endif //MODULES_TASK_1_GOSTEEVA_E_DIJKSTRA_DIJKSTRA_H
