40f04d33562ce3937c78f4d7b4e065bc062e6a92
Kat-G <katylaky@outlook.com>
2023-03-30 15:54:28 +0300

    c3

--- modules/task_1/gosteeva_e_dijkstra/dijkstra.cpp
+++ modules/task_1/gosteeva_e_dijkstra/dijkstra.cpp
@@ -2,39 +2,30 @@
 
 #include "../../../modules/task_1/gosteeva_e_dijkstra/dijkstra.h"
 
-std::vector<int> Dijkstra(std::vector<int> graph, int source,int size)
-{
+std::vector<int> Dijkstra(std::vector<int> graph, int source, int size) {
 	std::vector<int> dist(size);                       
-	std::vector<bool> visited(size);
-
-	int min = INT_MAX, index; 
-	for (int i = 0; i < size; i++)
-	{
-		dist.at(i) = INT_MAX;
-		visited.at(i) = false;
-	}
-
-	dist.at(source) = 0;
-
-	for (int i = 0; i < size; i++)
-	{
-		min = INT_MAX;
-		for (int i = 0; i < size; i++)
-		{
-			if (visited.at(i) == false && dist.at(i) <= min)
-			{
-				min = dist.at(i);
-				index = i;
+    std::vector<bool> visited(size);
+    int min = INT_MAX, index; 
+    for (int i = 0; i < size; i++) {
+        dist.at(i) = INT_MAX;
+        visited.at(i) = false;
+    }
+    dist.at(source) = 0;
+	for (int i = 0; i < size; i++) {
+        min = INT_MAX;
+        for (int i = 0; i < size; i++) {
+            if (visited.at(i) == false && dist.at(i) <= min) {
+                min = dist.at(i);
+                index = i;
+            }
+        }
+        visited.at(index) = true;
+        for (int i = 0; i < size; i++) {
+            if (!visited.at(i) && graph.at(index * size + i) && dist.at(index) != INT_MAX 
+            && dist.at(index) + graph.at(index * size + i) < dist.at(i)) {
+                dist.at(i) = dist.at(index) + graph.at(index * size + i);
 			}
-		}
-
-		visited.at(index) = true;
-		for (int i = 0; i < size; i++)
-		{
-			if (!visited.at(i) && graph.at(index * size + i) && dist.at(index) != INT_MAX && dist.at(index) + graph.at(index * size + i) < dist.at(i))
-				dist.at(i) = dist.at(index) + graph.at(index * size + i);
-		}
-	}
+        }
+    }
     return dist;
 }
-    //2,45 * 4,55
--- modules/task_1/gosteeva_e_dijkstra/dijkstra.h
+++ modules/task_1/gosteeva_e_dijkstra/dijkstra.h
@@ -1,10 +1,10 @@
 // Copyright 2023 Gosteeva Ekaterina
-#ifndef MODULES_TASK_1_GOSTEEVA_E_DIJKSTRA_DIJKSTRA_H
+#ifndef MODULES_TASK_1_GOSTEEVA_E_DIJKSTRA_DIJKSTRA_H_
 #define MODULES_TASK_1_GOSTEEVA_E_DIJKSTRA_DIJKSTRA_H
 
-#include <climits>     
+#include <climits>
 #include <vector>
 
-std::vector<int> Dijkstra(std::vector<int> graph, int source,int size);
+std::vector<int> Dijkstra(std::vector<int> graph, int source, int size);
 
-#endif //MODULES_TASK_1_GOSTEEVA_E_DIJKSTRA_DIJKSTRA_H
+#endif  // MODULES_TASK_1_GOSTEEVA_E_DIJKSTRA_DIJKSTRA_H_
