62a7e78ecf67eed0f875ac30e65cc002158d1bef
wellcome2AD <iuuyyttttt@mail.ru>
2023-03-30 21:06:12 +0300

    main fix

--- modules/task_1/korobeynikova_a_histogram_linear_stretching/main.cpp
+++ modules/task_1/korobeynikova_a_histogram_linear_stretching/main.cpp
@@ -6,21 +6,21 @@
 TEST(Sequential, Test_Image_1) {
   const Image example({255});
   Image image({255});
-  linearHistogramStretching(image);
+  linearHistogramStretching(&image);
   ASSERT_EQ(example, image);
 }
 
 TEST(Sequential, Test_Image_4) {
   const Image example({255, 0, 84, 122});
   Image image({128, 34, 65, 79});
-  linearHistogramStretching(image);
+  linearHistogramStretching(&image);
   ASSERT_EQ(example, image);
 }
 
 TEST(Sequential, Test_Image_9) {
   const Image example({214, 255, 145, 200, 170, 2, 242, 0, 214});
   Image image({183, 212, 133, 173, 151, 30, 203, 28, 183});
-  linearHistogramStretching(image);
+  linearHistogramStretching(&image);
   ASSERT_EQ(example, image);
 }
 
@@ -29,7 +29,7 @@ TEST(Sequential, Test_Image_16) {
                        35, 233, 142, 58});
   Image image({196, 40, 199, 105, 160, 158, 82, 165, 205, 107, 232, 240, 68,
                223, 152, 86});
-  linearHistogramStretching(image);
+  linearHistogramStretching(&image);
   ASSERT_EQ(example, image);
 }
 
@@ -39,7 +39,7 @@ TEST(Sequential, Test_Image_25) {
                        209, 235, 195, 131, 247, 184, 105});
   Image image({15, 202, 160, 34,  207, 147, 22,  156, 243, 97,  205, 222, 235,
                3,  3,   151, 108, 244, 201, 226, 188, 127, 237, 177, 103});
-  linearHistogramStretching(image);
+  linearHistogramStretching(&image);
   ASSERT_EQ(example, image);
 }
 
