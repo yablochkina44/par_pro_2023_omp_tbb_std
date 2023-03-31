7509f8824c9b696b7bbf8e6a0471015481869f0a
FrMks <84660461+FrMks@users.noreply.github.com>
2023-03-31 17:50:59 +0300

    first(3)

--- modules/task_1/frantcuzov_m_increasing_the_contrast/increasing_the_contrast.h
+++ modules/task_1/frantcuzov_m_increasing_the_contrast/increasing_the_contrast.h
@@ -1,6 +1,6 @@
 // Copyright 2023 Frantcuzov Maksim
-#ifndef MODULES_TASK_1_FRANTCUZOV_M_INCREASING_THE_CONTRAST_INCREASING_THE_CONTRAST
-#define MODULES_TASK_1_FRANTCUZOV_M_INCREASING_THE_CONTRAST_INCREASING_THE_CONTRAST
+#ifndef MODULES_TASK_1_FRANTCUZOV_M_INCREASING_THE_CONTRAST_INCREASING_THE_CONTRAST_H_
+#define MODULES_TASK_1_FRANTCUZOV_M_INCREASING_THE_CONTRAST_INCREASING_THE_CONTRAST_H_
 
 #include <vector>
 
@@ -12,4 +12,4 @@ std::vector<float> getSequentialOperations(const int width, const int height,
                                            const std::vector<float>& kernel,
                                            const std::vector<float>& img);
 
-#endif  // MODULES_TASK_1_FRANTCUZOV_M_INCREASING_THE_CONTRAST_INCREASING_THE_CONTRAST
+#endif  // MODULES_TASK_1_FRANTCUZOV_M_INCREASING_THE_CONTRAST_INCREASING_THE_CONTRAST_H_
