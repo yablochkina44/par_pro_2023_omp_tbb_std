4fe8861dba7cdf5d55d0cdd2d537c4e2c766edf9
Andrey Chubenko <81777187+snakeboby@users.noreply.github.com>
2023-04-01 16:12:01 +0300

    Small change to run workflow

--- modules/task_1/chubenko_a_dijkstra/main.cpp
+++ modules/task_1/chubenko_a_dijkstra/main.cpp
@@ -73,13 +73,13 @@ TEST(Dijkstra_Sequential, Test_Count_2) {
 TEST(Dijkstra_Sequential, Test_Count_7_Full) {
     const int size = 7;
     std::vector<int> graph = {0, 8, 1, 0, 1, 2, 0,
-                            8, 0, 1, 4, 7, 4, 6,
-                            1, 1, 0, 5, 4, 1, 0,
-                            0, 4, 5, 0, 3, 8, 7,
-                            1, 7, 4, 3, 0, 4, 9,
-                            2, 4, 1, 8, 4, 0, 1,
-                            0, 6, 0, 7, 9, 1, 0,
-                            };
+                             8, 0, 1, 4, 7, 4, 6,
+                             1, 1, 0, 5, 4, 1, 0,
+                             0, 4, 5, 0, 3, 8, 7,
+                             1, 7, 4, 3, 0, 4, 9,
+                             2, 4, 1, 8, 4, 0, 1,
+                             0, 6, 0, 7, 9, 1, 0,
+                             };
 
     const std::vector<int> expected = {0, 2, 1, 4, 1, 2, 3};
     std::vector<int> dists = get_dists_dijkstra(graph, size, 0);
