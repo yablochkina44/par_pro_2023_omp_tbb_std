6cf7d376a674b7d4a25238b03b6571b6f29f2544
Katherine <shaposhnikovakate@yandex.ru>
2023-04-02 19:30:15 +0300

    commit

--- modules/task_1/shaposhnikova_e_monte_carlo/main.cpp
+++ modules/task_1/shaposhnikova_e_monte_carlo/main.cpp
@@ -8,32 +8,32 @@ double func(double x, double y, double z) {
 }
 
 TEST(MonteCarloSeq, Test_1) {
-  std::vector<double> a{ 0,0,0 };
-  std::vector<double> b{ 1,1,1 };
+  std::vector<double> a{ 0, 0, 0 };
+  std::vector<double> b{ 1, 1, 1 };
   ASSERT_NO_THROW(MonteCarloSeq(a, b, 20000, func));
 }
 
 TEST(MonteCarloSeq, Test_2) {
-  std::vector<double> a{ 0,0,0 };
-  std::vector<double> b{ 1,1,1 };
+  std::vector<double> a{ 0, 0, 0 };
+  std::vector<double> b{ 1, 1, 1 };
   ASSERT_NEAR(MonteCarloSeq(a, b, 20000, func), 0.57, 0.1);
 }
 
 TEST(MonteCarloSeq, Test_3) {
-  std::vector<double> a{ 1,1,1 };
-  std::vector<double> b{ 5,5,5 };
+  std::vector<double> a{ 1, 1, 1 };
+  std::vector<double> b{ 5, 5, 5 };
   ASSERT_NO_THROW(MonteCarloSeq(a, b, 20000, func));
 }
 
 TEST(MonteCarloSeq, Test_4) {
-  std::vector<double> a{ 1,1,1 };
-  std::vector<double> b{ 5,5,5 };
+  std::vector<double> a{ 1, 1, 1 };
+  std::vector<double> b{ 5, 5, 5 };
   ASSERT_NEAR(MonteCarloSeq(a, b, 20000, func), 0.57, 0.1);
 }
 
 TEST(MonteCarloSeq, Test_5) {
-  std::vector<double> a{ 3,3,3 };
-  std::vector<double> b{ 7,7,7 };
+  std::vector<double> a{ 3, 3, 3 };
+  std::vector<double> b{ 7, 7, 7 };
   ASSERT_NO_THROW(MonteCarloSeq(a, b, 20000, func));
 }
 
