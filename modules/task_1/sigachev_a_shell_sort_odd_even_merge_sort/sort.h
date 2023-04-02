083f3db2ce0b51acfb91481fc43cb4b831c49270
SigachevAV <43341367+SigachevAV@users.noreply.github.com>
2023-04-02 22:00:10 +0300

    style fix 2

--- modules/task_1/sigachev_a_shell_sort_odd_even_merge_sort/sort.h
+++ modules/task_1/sigachev_a_shell_sort_odd_even_merge_sort/sort.h
@@ -8,4 +8,4 @@ std::vector<int> getRandomVector(int size);
 void ShellSort(std::vector<int>* vec);
 bool checkSort(const std::vector<int>& vec);
 
-#endif  // MODULES_TASK_1_SIGACHEV_A_SHELL_SORT_ODD_EVEN_MERGE_SORT_H_
+#endif  // MODULES_TASK_1_SIGACHEV_A_SHELL_SORT_ODD_EVEN_MERGE_SORT_SORT_H_
