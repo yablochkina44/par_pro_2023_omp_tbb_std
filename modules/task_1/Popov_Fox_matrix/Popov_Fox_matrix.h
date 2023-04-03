f992a0a981191a6ce41fb522f694043f07d43c42
Пашина Алина <nice.alina28@mail.ru>
2023-04-03 10:52:02 +0300

    first fix

--- modules/task_1/Popov_Fox_matrix/Popov_Fox_matrix.h
+++ modules/task_1/Popov_Fox_matrix/Popov_Fox_matrix.h
@@ -1,7 +1,4 @@
 // Copyright 2023 Popov Andrey
-#ifndef MODULES_TASK_1_Popov_Fox_matrix
-#define MODULES_TASK_1_Popov_Fox_matrix
-
 #include <vector>
 
 using matrix = std::vector<std::vector<double>>;
@@ -10,4 +7,3 @@ matrix simpleMult(const matrix& X, const matrix& Y);
 matrix foxMult(const matrix& X, const matrix& Y, const int& sizeb);
 matrix randMatrix(const int& cnt);
 
-#endif  // MODULES_TASK_1_Popov_Fox_matrix
