8a267593eb60212ea4765252e3e7dea89fe9e854
cherryBepis <119437132+cherryBepis@users.noreply.github.com>
2023-04-19 15:54:36 +0300

    Update main.cpp

--- modules/task_2/artemev_alexej_conj_grad/main.cpp
+++ modules/task_2/artemev_alexej_conj_grad/main.cpp
@@ -20,32 +20,17 @@ TEST(Conjugate_gradient, TEST_random_matrix3) {
     ASSERT_NE(emp, A);
 }
 
+TEST(Conjugate_gradient, TEST_random_matrix4) {
+    std::vector<std::vector<double>> A = random_matrix(6);
+    std::vector<std::vector<double>> emp;
+    ASSERT_NE(emp, A);
+}
 TEST(Conjugate_gradient, TEST_random_matrix2) {
     std::vector<std::vector<double>> A = random_matrix(5);
     for (int i = 0; i < 5; i++) {
         for (int j = i; j < 5; j++) {
             ASSERT_EQ(A[i][j], A[j][i]);
         }
-    }
-}
-
-
-TEST(Conjugate_gradient, TEST_1000x1000) {
-    int n = 11;
-    std::vector<std::vector<double>> A = random_matrix(1000);
-    std::vector<double> b = random_vec(1000);
-    clock_t start2 = clock();
-    std::vector<double> ResPar = par_conj_grad_method(A, b, n);
-    clock_t finish2 = clock();
-    clock_t start1 = clock();
-    std::vector<double> ResSec = conj_grad_method(A, b);
-    clock_t finish1 = clock();
-    double TimeSec = static_cast<double>(finish1 - start1) / CLOCKS_PER_SEC;
-    std::cout << "Sequental Time:" << TimeSec << std::endl;
-    double TimePar = static_cast<double>(finish2 - start2) / CLOCKS_PER_SEC;
-    std::cout << "Parallel Time:" << TimePar << std::endl;
-    for (int i = 0; i < 1000; i++) {
-        ASSERT_NEAR(ResSec[i], ResPar[i], 0.5);
     }
 }
 
