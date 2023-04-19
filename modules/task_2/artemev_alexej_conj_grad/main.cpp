967db8adaf0e3ed5d12db6abd039cde9f4b2256d
cherryBepis <119437132+cherryBepis@users.noreply.github.com>
2023-04-19 15:34:34 +0300

    Update main.cpp

--- modules/task_2/artemev_alexej_conj_grad/main.cpp
+++ modules/task_2/artemev_alexej_conj_grad/main.cpp
@@ -21,24 +21,7 @@ TEST(Conjugate_gradient, TEST_random_matrix2) {
     }
 }
 
-TEST(Conjugate_gradient, TEST_100x100) {
-    int n = 11;
-    std::vector<std::vector<double>> A = random_matrix(100);
-    std::vector<double> b = random_vec(100);
-    clock_t start2 = clock();
-    std::vector<double> ResPar = par_conj_grad_method(A, b, n);
-    clock_t finish2 = clock();
-    clock_t start1 = clock();
-    std::vector<double> ResSec = conj_grad_method(A, b);
-    clock_t finish1 = clock();
-    double TimeSec = static_cast<double>(finish1 - start1) / CLOCKS_PER_SEC;
-    std::cout << "Sequental Time:" << TimeSec << std::endl;
-    double TimePar = static_cast<double>(finish2 - start2) / CLOCKS_PER_SEC;
-    std::cout << "Parallel Time:" << TimePar << std::endl;
-    for (int i = 0; i < 100; i++) {
-        ASSERT_NEAR(ResSec[i], ResPar[i], 0.5);
-    }
-}
+
 TEST(Conjugate_gradient, TEST_1000x1000) {
     int n = 11;
     std::vector<std::vector<double>> A = random_matrix(1000);
