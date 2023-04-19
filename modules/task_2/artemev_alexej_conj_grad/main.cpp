f814477ec35835d32ff92f9002e4dd577fb45732
cherryBepis <119437132+cherryBepis@users.noreply.github.com>
2023-04-19 15:13:17 +0300

    Update main.cpp

--- modules/task_2/artemev_alexej_conj_grad/main.cpp
+++ modules/task_2/artemev_alexej_conj_grad/main.cpp
@@ -2,8 +2,25 @@
 #include <gtest/gtest.h>
 #include "./artemev_alexej_conj_grad.h"
 
-
-TEST(PAR_CONJ_GRAD, TEST_2x2) {
+TEST(Conjugate_gradient, TEST_random_vec) {
+    std::vector<double> a = random_vec(5);
+    std::vector<double> emp;
+    ASSERT_NE(emp, a);
+}
+TEST(Conjugate_gradient, TEST_random_matrix1) {
+    std::vector<std::vector<double>> A = random_matrix(5);
+    std::vector<std::vector<double>> emp;
+    ASSERT_NE(emp, A);
+}
+TEST(Conjugate_gradient, TEST_random_matrix2) {
+    std::vector<std::vector<double>> A = random_matrix(5);
+    for (int i = 0; i < 5; i++) {
+        for (int j = i; j < 5; j++) {
+            ASSERT_EQ(A[i][j], A[j][i]);
+        }
+    }
+}
+TEST(Conjugate_gradient, TEST_2x2) {
     int n = 2;
     std::vector<std::vector<double>> A = { { 3, 2}, {2, 4} };
     std::vector<double> b = { 1, 5 };
@@ -13,48 +30,7 @@ TEST(PAR_CONJ_GRAD, TEST_2x2) {
         ASSERT_NEAR(sol[i], res[i], 0.5);
     }
 }
-
-TEST(PAR_CONJ_GRAD, TEST_3x3) {
-    int n = 3;
-    std::vector<std::vector<double>> A = { { 3, 4, 0}, {4, -3, 0}, {0, 0, 5} };
-    std::vector<double> betta = { 1, 5, 9 };
-    std::vector<double> result = { 0.92, -0.44, 1.8 };
-    std::vector<double> decision = par_conj_grad_method(A, betta, n);
-
-    for (size_t i = 0; i < result.size(); ++i) {
-        ASSERT_NEAR(decision[i], result[i], 0.5);
-    }
-}
-
-TEST(PAR_CONJ_GRAD, TEST2_3x3) {
-    int n = 3;
-    std::vector<std::vector<double>> A = { { 3.2, -1.7, 2.5}, {1.5, 6.2, -2.9}, {2.8, -1.7, 3.8} };
-    std::vector<double> betta = { 3.7, 3.2, 2.8 };
-    std::vector<double> result = { 2.094, -0.465, -1.015 };
-    std::vector<double> decision = par_conj_grad_method(A, betta, n);
-
-    for (size_t i = 0; i < result.size(); ++i) {
-        ASSERT_NEAR(decision[i], result[i], 0.5);
-    }
-}
-
-TEST(PAR_CONJ_GRAD, TEST_5x5) {
-    int n = 4;
-    std::vector<std::vector<double>> A = { {5, 4, 0, 3, 6},
-                                           {4, 6, 1, 5, 7},
-                                           {0, 1, 0, 1, 1},
-                                           {3, 5, 1, 5, 6},
-                                           {6, 7, 1, 6, 10}, };
-    std::vector<double> betta = { 2, 5, 0, 9, 1 };
-
-    std::vector<double> x = par_conj_grad_method(A, betta, n);
-    std::vector<double> result = { 5, -2, 1, 9, -7 };
-    for (size_t i = 0; i < result.size(); ++i) {
-        ASSERT_NEAR(x[i], result[i], 0.5);
-    }
-}
-
-TEST(PAR_CONJ_GRAD, TEST2_5x5) {
+TEST(Conjugate_gradient, TEST_5x5) {
     int n = 5;
     std::vector<std::vector<double>> A = { {3, 2, 3, 1, 2},
                                            {2, 4, 1, 4, 3},
@@ -68,6 +44,43 @@ TEST(PAR_CONJ_GRAD, TEST2_5x5) {
         ASSERT_NEAR(x[i], res[i], 0.5);
     }
 }
+TEST(Conjugate_gradient, TEST_100x100) {
+    int n = 11;
+    std::vector<std::vector<double>> A = random_matrix(100);
+    std::vector<double> b = random_vec(100);
+    clock_t start2 = clock();
+    std::vector<double> ResPar = par_conj_grad_method(A, b, n);
+    clock_t finish2 = clock();
+    clock_t start1 = clock();
+    std::vector<double> ResSec = conj_grad_method(A, b);
+    clock_t finish1 = clock();
+    double TimeSec = static_cast<double>(finish1 - start1) / CLOCKS_PER_SEC;
+    std::cout << "Sequental Time:" << TimeSec << std::endl;
+    double TimePar = static_cast<double>(finish2 - start2) / CLOCKS_PER_SEC;
+    std::cout << "Parallel Time:" << TimePar << std::endl;
+    for (int i = 0; i < 100; i++) {
+        ASSERT_NEAR(ResSec[i], ResPar[i], 0.5);
+    }
+}
+TEST(Conjugate_gradient, TEST_1000x1000) {
+    int n = 11;
+    std::vector<std::vector<double>> A = random_matrix(1000);
+    std::vector<double> b = random_vec(1000);
+    clock_t start2 = clock();
+    std::vector<double> ResPar = par_conj_grad_method(A, b, n);
+    clock_t finish2 = clock();
+    clock_t start1 = clock();
+    std::vector<double> ResSec = conj_grad_method(A, b);
+    clock_t finish1 = clock();
+    double TimeSec = static_cast<double>(finish1 - start1) / CLOCKS_PER_SEC;
+    std::cout << "Sequental Time:" << TimeSec << std::endl;
+    double TimePar = static_cast<double>(finish2 - start2) / CLOCKS_PER_SEC;
+    std::cout << "Parallel Time:" << TimePar << std::endl;
+    for (int i = 0; i < 1000; i++) {
+        ASSERT_NEAR(ResSec[i], ResPar[i], 0.5);
+    }
+}
+
 
 
 int main(int argc, char** argv) {
