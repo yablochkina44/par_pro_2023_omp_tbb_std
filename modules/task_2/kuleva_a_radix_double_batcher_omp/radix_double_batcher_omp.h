77673aa3d97636c7550ca5237d5a17a2784ea3f0
AnnaKul5 <81855902+AnnaKul5@users.noreply.github.com>
2023-04-20 19:30:40 +0300

    initial

---
+++ modules/task_2/kuleva_a_radix_double_batcher_omp/radix_double_batcher_omp.h
@@ -0,0 +1,9 @@
+// Copyright 2023 Kuleva Anna
+#ifndef MODULES_TASK_2_KULEVA_A_RADIX_DOUBLE_BATCHER_OMP_RADIX_DOUBLE_BATCHER_OMP_H_
+#define MODULES_TASK_2_KULEVA_A_RADIX_DOUBLE_BATCHER_OMP_RADIX_DOUBLE_BATCHER_OMP_H_
+
+#include <vector>
+
+std::vector<double> radixBatcherSort(const std::vector<double>& base);
+
+#endif  // MODULES_TASK_2_KULEVA_A_RADIX_DOUBLE_BATCHER_OMP_RADIX_DOUBLE_BATCHER_OMP_H_
